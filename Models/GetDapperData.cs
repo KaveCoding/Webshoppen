4b97d8d25b3f39706ca8867451c8d930f99ae30a
KaveCoding <teachereli@outlook.com>
2022-12-22 11:17:58 +0100

    Servererror

---
+++ Models/GetDapperData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Demo_many2many2.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace DemoEFDapper
+{
+    public class GetDapperData
+    {
+        public static void Hämta_kategorier()
+        {
+            string connString = "data source=.\\Server=tcp:eliasanghnaeh.database.windows.net,1433;Initial Catalog=WebbshoppGrupp8Eskilstuna;Persist Security Info=False;User ID=Group8;Password=Ourpasswordis100%secure;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=5;";
+            
+            var sql = "SELECT * FROM Kategorier";
+
+            var kategorier = new List<Kategori>();
+
+            using (var connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                kategorier = connection.Query<Kategori>(sql).ToList();
+                connection.Close();
+
+            }
+
+            foreach( var x in kategorier)
+            {
+                Console.WriteLine(x.Namn, x.Id, x.Produkter);
+            }
+
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using EF_Demo_many2many2.Models;
+﻿using DemoEFDapper;
+using EF_Demo_many2many2.Models;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace EF_Demo_many2many2
 {
@@ -7,7 +9,33 @@ namespace EF_Demo_many2many2
     {
         static void Main(string[] args)
         {
+            //using (var db = new MyDBContext())
+            //{
+            //    var rnd = new Random();
+            //    var newCar = new Kategori
+            //    {
+            //        Namn = "Placeholder"
+            //    };
+            //    var carList = db.Kategorier;
+            //    carList.Add(newCar);
+            //    db.SaveChanges();
+            //}
 
+            //using (var db = new MyDBContext())
+            //{
+            //    var rnd = new Random();
+            //    var newCar = new Leverantör
+            //    {
+            //        Namn = "Placeholder",
+            //        Pris = 69,
+            //        LeveransTid = 24
+            //    };
+            //    var carList = db.Leverantörer;
+            //    carList.Add(newCar);
+            //    db.SaveChanges();
+            //}
+
+            GetDapperData.Hämta_kategorier();
         }
     }
 }
