6f6badfa407d1ca391fac14606fc82c72cc0d20e
KaveCoding <teachereli@outlook.com>
2023-01-16 17:40:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,19 +7,8 @@ namespace EF_Demo_many2many2
         static void Main(string[] args)
         {
 
-            //CRUD.InsertLagerStatus();
-            //CRUD.InsertBetalsätt();
-            //CRUD.InsertBeställningar();
-            //GetDapperData.Hämta_kategorier();
-            //CRUD.InsertKategori();
-            //CRUD.InsertLeverantör();
-            //CRUD.InsertProdukt();
-            //CRUD.Kund();
-            //CRUD.AdminDo();
-            //CRUD.UpdateKund();
-            //CRUD.UpdateProdukt();
-            //CRUD.VisaProdukter();
-            CRUD.VälkomstText();
+          
+            Meny.VälkomstText();
 
 
         }
